48f1deb36466eb7727753712fd2aef1967e88357
JayCodes <www.amujonathan@gmail.com>
2026-02-22 23:18:07 +0100

    feat(routing): configure application routes and add dynamic details route

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+// import Landing from "../pages/Landing";
+// import Dashboard from "../pages/Dashboard";
+import Applications from "../pages/Applications";
+import ApplicationDetails from "../pages/ApplicationDetails";
+// import FollowUps from "../pages/FollowUps";
+// import Contacts from "../pages/Contacts";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Landing />} /> */}
+      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      <Route path="/applications" element={<Applications />} />
+      <Route path="/applications/:id" element={<ApplicationDetails />} />
+      {/* <Route path="/follow-ups" element={<FollowUps />} /> */}
+      {/* <Route path="/contacts" element={<Contacts />} /> */}
+    </Routes>
+  );
+}
+
+export default AppRoutes;
