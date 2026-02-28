a8e1f336dfc8c9f8bda0f8f301963a2e8d28764f
JayCodes <www.amujonathan@gmail.com>
2026-02-28 17:33:52 +0100

    fix: remove dashboard page from router because it's not completed.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "../pages/Landing";
-import Dashboard from "../pages/Dashboard";
+// import Dashboard from "../pages/Dashboard";
 import Applications from "../pages/Applications";
 import ApplicationDetails from "../pages/ApplicationDetails";
 // import FollowUps from "../pages/FollowUps";
@@ -10,7 +10,7 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       <Route path="/applications" element={<Applications />} />
       <Route path="/applications/:id" element={<ApplicationDetails />} />
       {/* <Route path="/follow-ups" element={<FollowUps />} /> */}
